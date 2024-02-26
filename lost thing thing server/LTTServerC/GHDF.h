a4a5ee2d429ec5cba9b8fa8316172a978f172b9d
WOODEN CHEST <86293484+WOODEN-CHEST@users.noreply.github.com>
2024-02-26 08:38:31 +0200

    a
    a

--- lost thing thing server/LTTServerC/GHDF.h
+++ lost thing thing server/LTTServerC/GHDF.h
@@ -3,7 +3,30 @@
 #include <stddef.h>
 
 
+// Macros.
+#define GHDT_TYPE_ARRAY_BIT 0b100000000000000000000000000000000
+
+#define COMPOUND_DEFAULT_CAPACITY 32
+
+
 // Types.
+typedef enum GHDFTypeEnum
+{
+	GHDFType_SChar = 0,
+	GHDFType_UChar = 1,
+	GHDFType_Short =  2,
+	GHDFType_UShort = 3,
+	GHDFType_Int = 4,
+	GHDFType_UInt = 5,
+	GHDFType_Long = 6,
+	GHDFType_ULong = 7,
+	GHDFType_Float = 8,
+	GHDFType_Double = 9,
+	GHDFType_Bool = 10,
+	GHDFType_String = 11,
+	GHDFType_Compound = 12
+} GHDFType;
+
 union GHDFPrimitive
 {
 	signed char SChar;
@@ -21,6 +44,8 @@ union GHDFPrimitive
 	bool Bool;
 
 	char* String;
+
+	GHDFEntryStruct*
 };
 
 typedef struct GHDFArrayStruct
@@ -29,10 +54,29 @@ typedef struct GHDFArrayStruct
 	size_t Size;
 } GHDFArray;
 
-union GHDFEntry
+typedef union GHDFEntryValue
 {
-	GHDFPrimitive Value;
+	GHDFPrimitive SingleValue;
 	GHDFArray ValueArray;
+};
+
+typedef struct GHDFEntryStruct
+{
+	int ID;
+	GHDFType ValueType;
+	GHDFEntryValue Value;
+} GHDFEntry;
+
+typedef struct GHDFEntryBucket
+{
+	GHDFEntry* Entries;
+	size_t Count;
+	size_t _capacity;
+};
+
+typedef struct GHDFCompound
+{
+	GHDFEntry* Entries;
 };
 
 
